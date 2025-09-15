f927a898cc3ae162ba0c19d71409f886f69e5a09
beaujpick <beaujpick@gmail.com>
2025-09-15 04:28:59 +0000

    Done with labs

--- RPS.py
+++ RPS.py
@@ -1,13 +1,55 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name: Beau Pick
+#Date: 9-14-25
+#Assignment: RPS
 import random
 
 def main():
   wins = 0
   ties = 0
   losses = 0
+
+wins = 0
+ties = 0
+losses = 0
+
+
+print("Record: W/T/L", int(wins), int(ties), int(losses))
+
+while True:
+  choose = input("Enter your weapon (R for Rock, P for Paper, S for Scissors): ").upper()
+  if choose not in ("R", "P", "S"):
+      print ("Invalid, please choose R, P, or S.")
+      continue
+
+  output = random.choice (["R", "P", "S"])
+  print(f"Computer chose: {output}")
+
+  if choose == output:
+      print("You Tied!")
+      ties = ties + 1
+  elif (choose == "R" and output == "S") or \
+        (choose == "P" and output == "R") or \
+        (choose == "S" and output == "P"):
+      print("You Win!!")
+      wins = wins + 1
+  else:
+      print ("You Lose!!")
+      losses = losses + 1
+
+
+  print("Record: W/T/L", int(wins), int(ties), int(losses))
+
+ 
+  again = input ("Play Again? (Y/N): ").upper()
+  if again == "N":
+      break
+
+
+
+
+
+
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
 
@@ -17,9 +59,6 @@ def main():
   #Ask the user if they would like to play again.
 
   #In the end, print the stats
-  print("Wins \t Ties \t Losses")
-  print("---- \t ---- \t ------")
-  print(wins, "\t", ties , "\t", losses)
 
-if __name__ == '__main__':
-  main()
+
+
--- TempConvert.py
+++ TempConvert.py
@@ -1,16 +1,16 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name: Beau Pick
+#Date:9-14-25
+#Assignment:Temp Convert
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
+  tempF = float(input("Enter a Fahrenheiht temp: "))
   #Convert that temperature to celsius, rounding to 1 decimal percision
+  tempC = round((5/9) * (tempF - 32),1)
   #Output converted temperature.
-  tempF = 80
 
-  tempC = tempF / 2
 
   print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
